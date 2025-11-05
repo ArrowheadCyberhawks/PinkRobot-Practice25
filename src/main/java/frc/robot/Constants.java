64a94f15a1b260cc7c33d7de3152b45cf5891f92
DrewBoatXIII <28gleisner@arrowheadschools.org>
2025-11-04 19:32:36 -0600

    e

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,7 +2,7 @@ package frc.robot;
 
 public class Constants {
     public static final class Drive {
-        public static double slowModifier = 0.1;
+        public static double slowModifier = 0.8;
 
         
     }
