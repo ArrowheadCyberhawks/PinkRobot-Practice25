9fbb3835fbd99a8e955baa495fb3279fa7865266
Bryce M <sisubrycevp@gmail.com>
2025-11-04 19:37:13 -0600

    changed from tank to arcade in Drive.java lines 26-36

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,7 +2,7 @@ package frc.robot;
 
 public class Constants {
     public static final class Drive {
-        public static double slowModifier = 0.64; //slow modifier reduces sensitivity in joysticks.
+        public static double slowModifier = 0.84; //slow modifier reduces sensitivity in joysticks.
         // It slows down top speed, the smaller the decimal, the slower the reaction time
 
         
--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -26,14 +26,14 @@ public class Drive extends Command {
 		double leftPower;
 		double rightPower;
 		double xboxLY = Math.abs(IO.xboxDrive.getLeftY()) * IO.xboxDrive.getLeftY();
-		double xboxRY = -Math.abs(IO.xboxDrive.getRightY()) * IO.xboxDrive.getRightY();
+		double xboxRX = -Math.abs(IO.xboxDrive.getRightY()) * IO.xboxDrive.getRightY();
 		//makes it so that one set of wheels can go backwards so that it can turn
 
 
-		// leftPower = (xboxY - xboxX);
-		// rightPower = (xboxY + xboxX);
-		leftPower = xboxLY; 
-		rightPower = xboxRY; //slow modifier
+		leftPower = (xboxLY - xboxRX);
+		rightPower = (xboxLY + xboxRX);
+		//leftPower = xboxLY; 
+		//rightPower = xboxRY; //slow modifier
 		//assigning 
 
 		//System.out.println(leftPower + "   " + rightPower);
